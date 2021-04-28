64c4956af99797a3cea375e660bb9e7c01280795
Doug Branton <dbranton@stsci.edu>
2021-04-28 11:36:40 -0400

    start on a python client

---
+++ query_pixeldb.py
@@ -0,0 +1,28 @@
+import mysql.connector
+import pandas as pd
+import numpy as np
+
+class PixelDB:
+    def __init__(self,host,user,password):
+        """Define the pixel database connection creates a client connection and a cursor object"""
+        self.db = mysql.connector.connect(host=host,user=user,password=password)
+        self.cursor = self.db.cursor()
+
+    def query_pixel(self,pixel_row,pixel_col,date=None,anneal_num=None):
+        """Return pixel properties for a given pixel and anneal combination"""
+        if (not date) and (not anneal_num):
+            print("Please provide a date or an anneal number to retrieve pixel properties from.")
+            return
+        self.cursor.execute("[SQL STATEMENT HERE]")
+        for row in self.cursor:
+            print(row)
+
+    def query_anneal(self, date=None, anneal_num=None):
+        """Return anneal properties for a single anneal, probably want to handle querying for multiple anneals at once"""
+        if (not date) and (not anneal_num):
+            print(
+                "Please provide a date or an anneal number to identify an anneal to retrieve properties from")
+            return
+        self.cursor.execute("[SQL STATEMENT HERE]")
+        for row in self.cursor:
+            print(row)
