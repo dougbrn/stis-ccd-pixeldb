ac1cd26dc965cc16eb73a65fd780a564e1428439
Doug Branton <dougrbranton@gmail.com>
2021-05-01 11:29:03 -0400

    restructure into package structure

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name = 'stispixeldb',
+    description = 'Create and Query STIS Pixel History Database',
+    url = 'https://github.com/dougbrn/stis-ccd-pixeldb',
+    author = 'Doug Branton, Colton Parker, Eddie Woods',
+    author_email = 'dbranton@stsci.edu',
+    classifiers = ['Programming Language :: Python',
+                   'Intended Audience :: Science/Research',
+                   'Topic :: Scientific/Engineering :: Astronomy',
+                   'Topic :: Scientific/Engineering :: Physics',
+                   'Topic :: Software Development :: Libraries :: Python Modules'],
+    packages = find_packages(),
+    install_requires = ['pandas', 'numpy', 'mysql-connector-python'],
+    version = 0.0)
