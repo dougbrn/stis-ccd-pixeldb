9aba8724d181c72b5807c3122aa858dfaa7b7a84
kuroxgrl <56239975+kuroxgrl@users.noreply.github.com>
2021-05-02 02:01:01 -0400

    Update Pixel DB.sql
    mostly done, missing dark entity

--- Pixel DB.sql
+++ Pixel DB.sql
@@ -1,43 +1,53 @@
+CREATE DATABASE anneals_pixels;
+
 CREATE TABLE ANNEAL_PERIOD
 (
-    AnnealNumber INT NOT NULL,
+    AnnealNumber INT (4) PRIMARY KEY,
 	StartDate DATE,
     EndDate DATE,
-    NumberOfDarks INT NOT NULL,
-    PRIMARY KEY   (AnnealNumber)
-);
-
-CREATE TABLE PIXEL
-(
-    RowNum INT,
-	ColumnNum INT,
-    Detector_Name VARCHAR(20),
-    PRIMARY KEY   (RowNum, ColumnNum)
-);
-
-CREATE TABLE DETECTOR
-(
-    DName VARCHAR(10),
-	Annealed CHAR(10),
-    N_Pixels VARCHAR(10),
-    IName VARCHAR(10),
-    PRIMARY KEY   (DName)
+    NumberOfDarks INT
 );
 
 CREATE TABLE INSTRUMENT
 (
-    IName VARCHAR(10),
-	N_Detectors INT,
-    Functioning VARCHAR(4),
-    PRIMARY KEY   (IName)
+    IName VARCHAR(10) PRIMARY KEY,
+	N_Detectors INT (2),
+    Functioning VARCHAR(8)
 );
+
+CREATE TABLE DETECTOR
+(
+    DName VARCHAR(8) PRIMARY KEY,
+	Annealed CHAR(10),
+    N_Pixels INT(9),
+    IName VARCHAR(10),
+    FOREIGN KEY(IName) REFERENCES INSTRUMENT(IName) ON DELETE SET NULL
+);
+
+CREATE TABLE PIXEL
+(
+    RowNum INT (4),
+	ColumnNum INT (4),
+    Detector_Name VARCHAR(8),
+    PRIMARY KEY   (RowNum, ColumnNum),
+    FOREIGN KEY(Detector_Name) REFERENCES DETECTOR(DName) ON DELETE SET NULL
+);
+
+show full columns from pixel;
 
 CREATE TABLE HAS_PROPERTIES_IN
 (
-    AnnealNumber VARCHAR(10),
-	RowNum INT,
-    ColumnNum INT,
-    Stability INT,
-    DarkRate INT,
-    PRIMARY KEY   (AnnealNumber, Rownum, ColumnNum)
+    AnnealNumber INT(3),
+    RowNum INT (4),
+	ColumnNum INT (4),
+    Stability DECIMAL (9,4),
+    Sci_Mean DECIMAL (9,4),
+    Err_Mean DECIMAL (7,4),
+    NaN_Count INT (3),
+    Readnoise DECIMAL (6,4),
+    PRIMARY KEY (AnnealNumber, Rownum, ColumnNum),
+    FOREIGN KEY(AnnealNumber) REFERENCES ANNEAL_PERIOD(AnnealNumber) on delete CASCADE,
+    FOREIGN KEY(RowNum, ColumnNum) REFERENCES PIXEL(RowNum, ColumnNum) on delete CASCADE
+
 );
+
