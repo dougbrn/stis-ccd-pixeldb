ae577b72800bbfc07684abef26dd155d3293e716
Doug Branton <dougrbranton@gmail.com>
2021-05-02 16:22:54 -0400

    finish basic queries

--- stispixeldb/pixeldb.py
+++ stispixeldb/pixeldb.py
@@ -19,7 +19,7 @@ class PixelDB:
     def query_pixel(self,pixel_row,pixel_col,date=None,anneal_num=None):
         """Return pixel properties for a given pixel and anneal combination"""
         if (not date) and (not anneal_num):
-            print("Please provide a date (format YYYY-MM-DD) or an anneal number to retrieve pixel properties from.")
+            print("Please provide a date (format YYYY-MM-DD or YYYY-MM-DD HH:MM:SS) or an anneal number to retrieve pixel properties from.")
             return
         elif date and not anneal_num:
             anneal_num = date_to_anneal_num(date)
@@ -33,19 +33,25 @@ class PixelDB:
     def query_anneal(self, date=None, anneal_num=None, instrument='STIS', detector='CCD'):
         """Return anneal properties for a single anneal, probably want to handle querying for multiple anneals at once"""
         if (not date) and (not anneal_num):
-            print(
-                "Please provide a date or an anneal number to identify an anneal to retrieve properties from")
+            print("Please provide a date (format YYYY-MM-DD or YYYY-MM-DD HH:MM:SS) or an anneal number to retrieve pixel properties from.")
             return
-        result = self.__execute("[SQL STATEMENT HERE]")
+        elif date and not anneal_num:
+            anneal_num = date_to_anneal_num(date)
+        sql_statement = f"SELECT * FROM ANNEAL_PERIOD \
+                        WHERE ANNEALNUMBER = {anneal_num}"
+        result = self.__execute(sql_statement)
         return
 
     def query_anneal_darks(self, date=None, anneal_num=None, instrument='STIS', detector='CCD'):
         """Return the list of dark names for a given anneal"""
         if (not date) and (not anneal_num):
-            print(
-                "Please provide a date or an anneal number to identify an anneal to retrieve dark file names from")
+            print("Please provide a date (format YYYY-MM-DD or YYYY-MM-DD HH:MM:SS) or an anneal number to retrieve pixel properties from.")
             return
-        result = self.__execute("[SQL STATEMENT HERE]")
+        elif date and not anneal_num:
+            anneal_num = date_to_anneal_num(date)
+        sql_statement = f"SELECT * FROM DARKS \
+                        WHERE ANNEALNUMBER = {anneal_num}"
+        result = self.__execute(sql_statement)
         return
 
     def check_for_anneals(self, exclude=[]):
